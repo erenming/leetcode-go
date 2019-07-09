9832589567c3604367ace1b115a44fb93e3d953a
tomming233 <tomming233@gmail.com>
2019-07-09 23:36:45 +0800

    move Zeroes

---
+++ arrays/MoveZeroes.go
@@ -0,0 +1,14 @@
+package arrays
+
+func moveZeroes(nums []int)  {
+	index := 0
+	for _, i := range nums {
+		if i != 0 {
+			nums[index] = i
+			index++
+		}
+	}
+	for ; index < len(nums); index++ {
+		nums[index] = 0
+	}
+}
