57ec03ebf8361f234a890089333fbeaad602c8bb
tomming233 <tomming233@gmail.com>
2020-01-12 14:24:33 +0800

    problem 215

---
+++ company/alibaba/findKthLargest/findKthLargest.py
@@ -0,0 +1,18 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        h = []
+        for i in nums:
+            if len(h) < k:
+                heapq.heappush(h, i)
+                continue
+            if i > h[0]:
+                heapq.heappushpop(h, i)
+                continue
+        return h[0]
+
+if __name__ == '__main__':
+    Solution().findKthLargest([3,2,1,5,6,4], 2)
