fd683a4c0ea0e411569810596659e5fac2557000
tomming233 <tomming233@gmail.com>
2020-01-13 13:35:17 +0800

    problem 19

---
+++ company/alibaba/removeNthFromEnd/removeNthFromEnd.go
@@ -0,0 +1,21 @@
+package removeNthFromEnd
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{-1, head}
+	fast := dummy
+	slow := dummy
+	for i := 0; i < n+1; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
---
+++ company/alibaba/removeNthFromEnd/removeNthFromEnd_test.go
@@ -0,0 +1,27 @@
+package removeNthFromEnd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_removeNthFromEnd(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"t-1", args{&ListNode{1, nil}, 1}, &ListNode{1, nil}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
