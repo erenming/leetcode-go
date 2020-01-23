e6f6bf9fdf8368a567391d816713b99584fa68f7
tomming233 <tomming233@gmail.com>
2020-01-23 11:38:05 +0800

    problem 62

---
+++ company/alibaba/uniquePaths/uniquePaths.go
@@ -0,0 +1,18 @@
+package uniquePaths
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		tmp := make([]int, n)
+		for j := 0; j < n; j++ {
+			tmp[j] = 1
+		}
+		dp[i] = tmp
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
