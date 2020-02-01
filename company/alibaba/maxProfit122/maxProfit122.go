65ba7bdc9f265615d31703a2e14cedd872174353
tomming233 <tomming233@gmail.com>
2020-02-01 11:45:36 +0800

    problem 122

--- company/alibaba/maxProfit122/maxProfit122.go
+++ company/alibaba/maxProfit122/maxProfit122.go
@@ -1,5 +1,11 @@
 package maxProfit122
 
 func maxProfit(prices []int) int {
-
+	profit, n := 0, len(prices)
+	for i := 0; i < n - 1; i ++ {
+		if prices[i+1] > prices[i] {
+			profit += prices[i+1] - prices[i]
+		}
+	}
+	return profit
 }
